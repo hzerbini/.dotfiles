98c5fe89acc03aaf048a2a5a71ccbe62fafd9df0
Henrique Zerbini Junqueira de Assis <henriquezjassis@gmail.com>
2023-10-12 17:50:46 -0300

    chore: forgot changes

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,13 +1,13 @@
-local telescope = require('telescope')
-local builtin = require('telescope.builtin')
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
 
-telescope.setup {
+telescope.setup({
     defaults = {
         mappings = {
             i = {
-                ["<C-q>"] = "smart_send_to_qflist"
-            }
-        }
+                ["<C-q>"] = "smart_send_to_qflist",
+            },
+        },
     },
     extensions = {
         file_browser = {
@@ -23,21 +23,38 @@ telescope.setup {
             },
         },
     },
-}
+})
 
-telescope.load_extension "file_browser"
-telescope.load_extension('dap')
-telescope.load_extension('git_worktree')
+telescope.load_extension("file_browser")
+telescope.load_extension("dap")
+telescope.load_extension("git_worktree")
 
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set("n", "<leader>ps", function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", {})
-vim.keymap.set('n', '<leader>fc', "<cmd>Telescope git_commits<cr>", {})
-vim.keymap.set('n', '<leader>fe', ':Telescope file_browser path=%:p:h select_buffer=true<cr>', { noremap = true })
-vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files hidden=true<cr>", {})
-vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", {})
-vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<cr>", {})
-vim.keymap.set('n', '<leader>fw', "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", {})
-vim.keymap.set('n', '<leader>f%', "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", {})
-vim.keymap.set('n', '<leader>fk', "<cmd>Telescope keymaps<cr>", {})
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {})
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope git_commits<cr>", {})
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", {})
+vim.keymap.set(
+    "n",
+    "<leader>fe",
+    ":Telescope file_browser path=%:p:h select_buffer=true<cr>",
+    { noremap = true }
+)
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", {})
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {})
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {})
+vim.keymap.set("n", "<leader>f^", "<cmd>Telescope resume<cr>", {})
+vim.keymap.set(
+    "n",
+    "<leader>fw",
+    "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>",
+    {}
+)
+vim.keymap.set(
+    "n",
+    "<leader>f%",
+    "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>",
+    {}
+)
+vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", {})
