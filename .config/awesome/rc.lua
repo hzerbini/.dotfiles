b636053106b5411a8d9e5f8a235898f06d380de2
Henrique Zerbini Junqueira de Assis <henriquezjassis@gmail.com>
2024-07-11 19:36:24 -0300

    feature: automatic generation of tmux session (alacritty)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -296,6 +296,9 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
 	awful.key({ modkey }, "s", hotkeys_popup.show_help, { description = "show help", group = "awesome" }),
+	awful.key({ modkey }, "/", function()
+		awful.spawn.with_shell(terminal .. " -e /home/zerbini/Scripts/cht.sh")
+	end, { description = "open tmux session creator", group = "launcher" }),
 	awful.key({ modkey }, "Left", awful.tag.viewprev, { description = "view previous", group = "tag" }),
 	awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
 	awful.key({ modkey }, "Escape", awful.tag.history.restore, { description = "go back", group = "tag" }),
