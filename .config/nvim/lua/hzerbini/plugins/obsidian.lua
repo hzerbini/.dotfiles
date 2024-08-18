3ab80685891998e606cfcbe299fff95072c756b8
Henrique Zerbini Junqueira de Assis <henriquezjassis@gmail.com>
2024-08-18 12:00:17 -0300

    feature: add brave bookmarks to configs

--- .config/nvim/lua/hzerbini/plugins/obsidian.lua
+++ .config/nvim/lua/hzerbini/plugins/obsidian.lua
@@ -360,7 +360,7 @@ return {
 
         -- Optional, configure additional syntax highlighting / extmarks.
         -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
-        conceallevel = 1,
+        conceallevel = 2,
         ui = {
             enable = true, -- set to false to disable all additional syntax features
             update_debounce = 200, -- update delay after a text change (in milliseconds)
